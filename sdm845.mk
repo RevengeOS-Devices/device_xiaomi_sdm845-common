51e466d77ff4276421e35932064df4078a0bcd7b
Bruno Martins <bgcngm@gmail.com>
2020-01-26 00:01:13 +0000

    sdm845-common: Update destination path for HotwordEnrollment permission file
    HotwordEnrollment has been moved to /product in Q.
    
    Change-Id: Ifc30940dcfc6ef236ed507ddfec1189b7e4f547b

--- sdm845.mk
+++ sdm845.mk
@@ -197,7 +197,7 @@ PRODUCT_PACKAGES += \
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
+    $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
     
 # Hotspot
 PRODUCT_COPY_FILES += \
