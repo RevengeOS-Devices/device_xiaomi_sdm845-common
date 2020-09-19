ddae7935005f6456656ad9cf99a5fc1fca030b11
Bruno Martins <bgcngm@gmail.com>
2020-09-19 20:06:49 +0100

    sdm845-common: Make IFAAManager compile again
    Change-Id: Iae2d1004bd1b3c939b552f09ea994ae87d71d73e

--- org.ifaa.android.manager/src/org/ifaa/android/manager/IFAAManagerFactory.java
+++ org.ifaa.android.manager/src/org/ifaa/android/manager/IFAAManagerFactory.java
@@ -1,8 +1,10 @@
 package org.ifaa.android.manager;
 
+import android.compat.annotation.UnsupportedAppUsage;
 import android.content.Context;
 
 public class IFAAManagerFactory {
+    @UnsupportedAppUsage
     public static IFAAManager getIFAAManager(Context context, int authType) {
         return IFAAManagerImpl.getInstance(context);
     }
