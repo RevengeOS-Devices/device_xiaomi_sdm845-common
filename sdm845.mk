e8d4843e4dc370973a835eb12ee050fb15cb8829
Arian <arian.kulmer@web.de>
2020-09-22 17:32:07 +0200

    sdm845-common: Add a shim for dpmframework
    * libdpmframework depends on strndup16to8 and strdup8to16 methods.
    
    Change-Id: I39d252b5830b752728a0c8607d32d8a279524ce1
    Co-authored-by: Demon000 <demonsingur@gmail.com>
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -181,6 +181,10 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.9.vendor \
     vendor.qti.hardware.display.allocator@1.0-service
 
+# DPM
+PRODUCT_PACKAGES += \
+    libshim_dpmframework
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
