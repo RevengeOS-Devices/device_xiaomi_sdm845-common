f1bfb607817ae6f8cf94243649f20945951db417
Chenyang Zhong <zhongcy95@gmail.com>
2020-10-10 06:14:25 -0400

    sdm845-common: libperfmgr: add soong namespace for aidl power hal
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -341,6 +341,7 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
     hardware/google/pixel \
     vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
     vendor/qcom/opensource/commonsys/system/bt/conf
